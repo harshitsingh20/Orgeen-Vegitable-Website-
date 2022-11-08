a715b14b7fa9ab471301133336ab6533adbe4eae
Harshit Singh <73012345+harshitsingh20@users.noreply.github.com>
2022-11-08 09:08:16 +0530

    sort

---
+++ sort.js
@@ -0,0 +1,13 @@
+<script>
+// JavaScript to illustrate sort() function
+function func() {
+ 
+    // Original string
+    var arr = ["Geeks", "for", "Geeks"]
+ 
+    console.log(arr);
+    // Sorting the array
+    console.log(arr.sort());
+}
+func();
+</script>
